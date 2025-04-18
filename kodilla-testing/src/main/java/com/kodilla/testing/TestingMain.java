e8c97eab6b54b74d40b998c14fa365036dc3956c
Viktoriia Fortuna <fortuna.vika@onet.pl>
2025-04-18 12:07:56 +0200

    6.2 Added Calculator class and test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,8 +1,34 @@
 package com.kodilla.testing;
 
-public class TestingMain {
-    public static void main(String[] args) {
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
 
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+public class TestingMain {
+
+    public static void main(String[] args) {
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+        // tu umieść kolejny test ->
+        System.out.println("Test - pierwszy test jednostkowy:");
+
+        //...
+        System.out.println("Test- drugi test do kalkulatora");
+        Calculator calculator = new Calculator();
+        int resultAdd = calculator.add(2, 3);
+        int resultSubtract = calculator.subtract(3, 2);
+        if ((resultAdd == 5) && (resultSubtract == 1)) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+
     }
 }
